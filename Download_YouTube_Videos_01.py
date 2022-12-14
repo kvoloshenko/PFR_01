7ede6679f276fb5c2aba615a76082bf8c5b86a40
kvoloshenko <kvoloshenko@gmail.com>
2022-12-14 07:06:59 +0300

    Refactoring

--- Download_YouTube_Videos_01.py
+++ Download_YouTube_Videos_01.py
@@ -14,7 +14,23 @@ def Download(link):
         print("An error has occurred")
     print("Download is completed successfully")
 
-url = 'https://youtu.be/8Ramec1_RNU'
-
-
-Download(url)
+urls = ['https://youtu.be/OJRN1Lmwjuw',
+        'https://youtu.be/UO1fTaVTClk',
+        'https://youtu.be/UuBuojnty6o',
+        'https://youtu.be/deabMSgWt4Q',
+        'https://youtu.be/mCN1NLrSjiE',
+        'https://youtu.be/YJ3Hn_F2diE',
+        'https://youtu.be/CA3K-HUlg-A',
+        'https://youtu.be/VSc8uIpiamw',
+        'https://youtu.be/ryxicetVgB4',
+        'https://youtu.be/eiqjnzmpiFE',
+        'https://youtu.be/hQEKSSd18Zc',
+        'https://youtu.be/L7uceNko_Ig',
+        'https://youtu.be/tKZ4slDuKmE',
+        'https://youtu.be/RXORG4S1KkA',
+        'https://youtu.be/LBiCA5lCi_k']
+i = 1
+for url in urls:
+    print(f'i={i}, url={url}')
+    i += 1
+    Download(url)

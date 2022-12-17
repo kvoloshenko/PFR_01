07a8cf6432a9570377017d3f7111dec3f7a82d42
kvoloshenko <kvoloshenko@gmail.com>
2022-12-17 12:11:06 +0300

    Refactoring

--- Face_Recognition_050.py
+++ Face_Recognition_050.py
@@ -8,14 +8,27 @@ import random
 import numpy as np
 from matplotlib import pyplot as plt
 # Import tensorflow dependencies - Functional API
-import tensorflow
-keras = tensorflow.keras
+import tensorflow as tf
+keras = tf.keras
 from tensorflow.keras.models import Model
 from tensorflow.keras.layers import Layer, Conv2D, Dense, MaxPooling2D, Input, Flatten
-import tensorflow as tf
+
 # Avoid OOM errors by setting GPU Memory Consumption Growth
 gpus = tf.config.experimental.list_physical_devices('GPU')
 print (type(gpus), f'gpus ={gpus}')
 for gpu in gpus:
     tf.config.experimental.set_memory_growth(gpu, True)
     print (type(gpu), f'gpu ={gpu}')
+
+# Setup paths
+DATA_PATH = 'FR_05_data'
+POS_PATH = os.path.join(DATA_PATH, 'positive')
+NEG_PATH = os.path.join(DATA_PATH, 'negative')
+ANC_PATH = os.path.join(DATA_PATH, 'anchor')
+# Make the directories
+if not (os.path.exists(POS_PATH)):
+    os.makedirs(POS_PATH)
+    os.makedirs(NEG_PATH)
+    os.makedirs(ANC_PATH)
+
+# https://youtu.be/LKispFFQ5GU?t=1518

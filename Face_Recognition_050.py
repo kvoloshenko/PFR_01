ca19d67fa564b3871a91c945ed3a7d19a3689289
kvoloshenko <kvoloshenko@gmail.com>
2022-12-17 16:18:54 +0300

    Refactoring

--- Face_Recognition_050.py
+++ Face_Recognition_050.py
@@ -32,3 +32,57 @@ if not (os.path.exists(POS_PATH)):
     os.makedirs(ANC_PATH)
 
 # https://youtu.be/LKispFFQ5GU?t=1518
+
+# Untar Labelled Faces in the Wild Dataset
+# http://vis-www.cs.umass.edu/lfw/
+# Uncompress Tar GZ Labelled Faces in the Wild Dataset
+# !tar -xf lfw.tgz
+
+# Move LFW Images to the following repository data/negative
+# for directory in os.listdir('lfw'):
+#     for file in os.listdir(os.path.join('lfw', directory)):
+#         EX_PATH = os.path.join('lfw', directory, file)
+#         NEW_PATH = os.path.join(NEG_PATH, file)
+#         os.replace(EX_PATH, NEW_PATH)
+
+# Collect Positive and Anchor Classes
+# ttps://youtu.be/LKispFFQ5GU?t=2603
+# Import uuid library to generate unique image names
+import uuid
+
+os.path.join(ANC_PATH, '{}.jpg'.format(uuid.uuid1()))
+# Establish a connection to the webcam
+cap = cv2.VideoCapture(0)
+
+while cap.isOpened():
+    ret, frame = cap.read()
+
+    # Cut down frame to 250x250px
+    frame = frame[120:120 + 250, 200:200 + 250, :]
+
+    # Collect anchors
+    if cv2.waitKey(1) & 0XFF == ord('a'):
+        # Create the unique file path
+        imgname = os.path.join(ANC_PATH, '{}.jpg'.format(uuid.uuid1()))
+        # Write out anchor image
+        cv2.imwrite(imgname, frame)
+
+    # Collect positives
+    if cv2.waitKey(1) & 0XFF == ord('p'):
+        # Create the unique file path
+        imgname = os.path.join(POS_PATH, '{}.jpg'.format(uuid.uuid1()))
+        # Write out positive image
+        cv2.imwrite(imgname, frame)
+
+    # Show image back to screen
+    cv2.imshow('Image Collection', frame)
+
+    # Breaking gracefully
+    if cv2.waitKey(1) & 0XFF == ord('q'):
+        break
+
+# # Release the webcam
+# cap.release()
+# # Close the image show frame
+# cv2.destroyAllWindows()
+# plt.imshow(frame[120:120 + 250, 200:200 + 250, :])

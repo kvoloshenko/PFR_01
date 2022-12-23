a70cd6eb05cf3cccc90e602efaadfea0cec7eac5
kvoloshenko <kvoloshenko@gmail.com>
2022-12-24 02:27:47 +0300

    Refactoring

--- Face_Detection_020.py
+++ Face_Detection_020.py
@@ -305,9 +305,108 @@ print(f'classes={classes}, coords={coords}')
 # 9. Define Losses and Optimizers
 # 9.1 Define Optimizer and LR
 # https://youtu.be/N_W4EYtsa10?t=6082
+print(f'len(train)={len(train)}')
 batches_per_epoch = len(train)
 lr_decay = (1./0.75 -1)/batches_per_epoch
 opt = tf.keras.optimizers.Adam(learning_rate=0.0001, decay=lr_decay)
+
+# 9.2 Create Localization Loss and Classification Loss
+# https://youtu.be/N_W4EYtsa10?t=6174
+def localization_loss(y_true, yhat):
+    delta_coord = tf.reduce_sum(tf.square(y_true[:, :2] - yhat[:, :2]))
+    h_true = y_true[:, 3] - y_true[:, 1]
+    w_true = y_true[:, 2] - y_true[:, 0]
+    h_pred = yhat[:, 3] - yhat[:, 1]
+    w_pred = yhat[:, 2] - yhat[:, 0]
+    delta_size = tf.reduce_sum(tf.square(w_true - w_pred) + tf.square(h_true - h_pred))
+    return delta_coord + delta_size
+
+classloss = tf.keras.losses.BinaryCrossentropy()
+regressloss = localization_loss
+
+# 9.3 Test out Loss Metrics
+# https://youtu.be/N_W4EYtsa10?t=6297
+print(f'localization_loss={localization_loss(y[1], coords)}')
+print(f'classloss={classloss(y[0], classes)}')
+print(f'regressloss={regressloss(y[1], coords)}')
+
+# 10. Train Neural Network
+# 10.1 Create Custom Model Class
+#
+class FaceTracker(Model):
+    def __init__(self, eyetracker, **kwargs):
+        super().__init__(**kwargs)
+        self.model = eyetracker
+
+    def compile(self, opt, classloss, localizationloss, **kwargs):
+        super().compile(**kwargs)
+        self.closs = classloss
+        self.lloss = localizationloss
+        self.opt = opt
+
+    def train_step(self, batch, **kwargs):
+        X, y = batch
+
+        with tf.GradientTape() as tape:
+            classes, coords = self.model(X, training=True)
+
+            batch_classloss = self.closs(y[0], classes)
+            batch_localizationloss = self.lloss(tf.cast(y[1], tf.float32), coords)
+
+            total_loss = batch_localizationloss + 0.5 * batch_classloss
+
+            grad = tape.gradient(total_loss, self.model.trainable_variables)
+
+        opt.apply_gradients(zip(grad, self.model.trainable_variables))
+
+        return {"total_loss": total_loss, "class_loss": batch_classloss, "regress_loss": batch_localizationloss}
+
+    def test_step(self, batch, **kwargs):
+        X, y = batch
+
+        classes, coords = self.model(X, training=False)
+
+        batch_classloss = self.closs(y[0], classes)
+        batch_localizationloss = self.lloss(tf.cast(y[1], tf.float32), coords)
+        total_loss = batch_localizationloss + 0.5 * batch_classloss
+
+        return {"total_loss": total_loss, "class_loss": batch_classloss, "regress_loss": batch_localizationloss}
+
+    def call(self, X, **kwargs):
+        return self.model(X, **kwargs)
+
+
+model = FaceTracker(facetracker)
+model.compile(opt, classloss, regressloss)
+# print (model.summary())
+# f_plot_model(model) # Выводим схему модели
+
+# 10.2 Train
+logdir='FD_02_logs'
+tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=logdir)
+hist = model.fit(train, epochs=10, validation_data=val, callbacks=[tensorboard_callback])
+
+# 10.3 Plot Performance
+hist.history
+fig, ax = plt.subplots(ncols=3, figsize=(20,5))
+
+ax[0].plot(hist.history['total_loss'], color='teal', label='loss')
+ax[0].plot(hist.history['val_total_loss'], color='orange', label='val loss')
+ax[0].title.set_text('Loss')
+ax[0].legend()
+
+ax[1].plot(hist.history['class_loss'], color='teal', label='class loss')
+ax[1].plot(hist.history['val_class_loss'], color='orange', label='val class loss')
+ax[1].title.set_text('Classification Loss')
+ax[1].legend()
+
+ax[2].plot(hist.history['regress_loss'], color='teal', label='regress loss')
+ax[2].plot(hist.history['val_regress_loss'], color='orange', label='val regress loss')
+ax[2].title.set_text('Regression Loss')
+ax[2].legend()
+
+plt.show()
+
 
 
 

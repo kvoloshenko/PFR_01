73c974e7061d0fe2b37309b89e3f4fe755d30e85
kv <kvoloshenko@gmail.com>
2022-12-31 00:27:14 +0300

    Refactoring

--- Check_Tf_01.py
+++ Check_Tf_01.py
@@ -1,4 +1,7 @@
 import tensorflow as tf
+import os
+print(os.environ.get('LD_LIBRARY_PATH'))
+# /usr/local/cuda/lib64:/home/kv/miniconda3/lib/:/home/kv/miniconda3/envs/tf/lib/
 tf.config.list_physical_devices('GPU')
 print("Num GPUs Available: ", len(tf.config.experimental.list_physical_devices('GPU')))
 tf.test.gpu_device_name()
